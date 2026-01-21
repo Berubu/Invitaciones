b01109055d7571bf0e34d264c80b9f1bd42118fe
BERU <93852000+Berubu@users.noreply.github.com>
2026-01-20 22:48:17 -0600

    1

---
+++ 1 Boda/script.js
@@ -0,0 +1,7 @@
+function abrirCarta() {
+    // Oculta el sobre
+    document.getElementById('sobre-container').style.display = 'none';
+    
+    // Muestra el contenido de la boda
+    document.getElementById('contenido-boda').classList.remove('hidden');
+}
